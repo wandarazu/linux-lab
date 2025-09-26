7eeb0bcc7a85fa628f1dfe84b97f58b7fa0122ec
wandarazu <zedleft2@mail.ru>
2025-09-26 13:40:09 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // Uncomment this block to pass the first stage
+  std::cout << "$ ";
+
+  std::string input;
+  while(true){
+    std::getline(std::cin, input);
+  std::cout << input << ": command not found" << std::endl;
+  }
+}
+  
